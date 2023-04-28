2aba2b72242c5deb7ee23343bf144d7c4c320696
gretazavagno <g.zavagno1@gmail.com>
2023-04-28 23:11:47 +0200

    progetto04

--- settimana1/progetto04/assets/js/script.js
+++ settimana1/progetto04/assets/js/script.js
@@ -11,16 +11,48 @@ window.addEventListener('scroll', function(){
         nav.classList.add('scroll');
     }else{
         nav.classList.remove('coloreNavbar');
-        btn.classList.remove('coloreBtn');
+        btn.classList.remove('colorBtn');
+        nav.classList.remove('scroll');
+
     }
 });
 
-// window.addEventListener('scroll', function() {
-//     var nav = document.querySelector('nav');
+// const path = document.querySelectorAll('path[id="p"]');
+
+// path.forEach((p) => {
+//     setInterval(() => {
+//         if(Math.random() < 0.3){
+//             p.style.display = 'block';
+//         }else{
+//             p.style.display = 'none'
+//         }
+//     }, 2000)
+// })
+
+const paths = Array.from(document.querySelectorAll("#p"));
+
+function hideAndShowRandomPaths() {
+    // Numero di path che devono essere nascosti
+    const numberOfPathsToHide = Math.floor(Math.random() * 40);
     
-//     if (distanceFromTop >= 200) {
-//       nav.classList.add('scrolled');
-//     } else {
-//       nav.classList.remove('scrolled');
-//     }
-//   });
+    // Array contenente gli indici dei path da nascondere
+    const pathsToHideIndexes = [];
+  
+    while (pathsToHideIndexes.length < numberOfPathsToHide) {
+      const randomIndex = Math.floor(Math.random() * paths.length);
+  
+      // Verifica che l'indice non sia già presente nell'array
+      if (!pathsToHideIndexes.includes(randomIndex)) {
+        pathsToHideIndexes.push(randomIndex);
+        paths[randomIndex].style.display = "none";
+        
+        // Tempo di attesa prima di far riapparire il path
+        const delay = Math.floor(Math.random() * 2000) + 1000;
+        setTimeout(() => {
+          paths[randomIndex].style.display = "block";
+        }, delay);
+      }
+    }
+  }
+
+  setInterval(hideAndShowRandomPaths, 1000);
