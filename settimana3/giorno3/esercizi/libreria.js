3579c7f4ad793d2e9d6613a47683223f43712609
gretazavagno <g.zavagno1@gmail.com>
2023-05-10 16:36:27 +0200

    es

--- settimana3/giorno3/esercizi/libreria.js
+++ settimana3/giorno3/esercizi/libreria.js
@@ -1,26 +1,50 @@
 const bookList = document.getElementById('book-list');
 
 fetch('https://striveschool-api.herokuapp.com/books')
+    
     .then(response => response.json())
-    .then(books => {
+    
+    .then(books => { //per ogni libro crea la card
         books.forEach(book => {
-            const bookDiv = document.createElement('div');
-            bookDiv.classList.add('book');
+            const col = document.createElement('div'); //variabile creazione colonna
+            col.classList.add('col-md-3', 'mb-4');
 
-            const title = document.createElement('h2');
-            title.textContent = book.title;
-
-            const author = document.createElement('p');
-            author.textContent = `Author: ${book.author}`;
+            const card = document.createElement('div');
+            card.classList.add('card');
 
             const img = document.createElement('img');
+            img.classList.add('card-img-top');
             img.src = book.img;
+            img.alt = book.title;
 
-            bookDiv.appendChild(title);
-            bookDiv.appendChild(author);
-            bookDiv.appendChild(img);
+            const cardBody = document.createElement('div');
+            cardBody.classList.add('card-body');
 
-            bookList.appendChild(bookDiv);
+            const title = document.createElement('h5');
+            title.classList.add('card-title');
+            title.textContent = book.title;
+
+            const price = document.createElement('p');
+            price.classList.add('card-text');
+            price.textContent = `Prezzo: ${book.price}`;
+
+            const button = document.createElement('button');
+            button.classList.add('btn', 'btn-danger');
+            button.textContent = 'Scarta';
+            button.addEventListener('click', () => {
+                col.remove();
+            });
+
+            cardBody.appendChild(title);
+            cardBody.appendChild(price);
+            cardBody.appendChild(button);
+
+            card.appendChild(img);
+            card.appendChild(cardBody);
+
+            col.appendChild(card);
+
+            bookList.appendChild(col);
         });
     })
     .catch(error => {
